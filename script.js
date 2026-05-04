74ebb00b62c21bc158a640aa9ce37f416b019153
norower <official5inco@gmail.com>
2026-05-03 22:38:34 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,90 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator = null;
+
+const screen = document.querySelector(".screen");
+
+function buttonClick(value) {
+    // IMPORTANTE: .trim() elimina los espacios que tienes en tu HTML
+    const cleanValue = value.trim();
+    
+    if (isNaN(cleanValue)) {
+        handleSymbol(cleanValue);
+    } else {
+        handleNumber(cleanValue);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            previousOperator = null;
+            break;
+        case '=':
+            if (previousOperator === null) return;
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = "" + runningTotal;
+            runningTotal = 0;
+            break;
+        case '←':
+            if (buffer.length === 1) {
+                buffer = '0';
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case '+':
+        case '−': // Signo menos largo
+        case '×': // Signo por
+        case '÷': // Signo división
+            handleMath(symbol);
+            break;
+    }
+}
+
+function handleMath(symbol) {
+    if (buffer === '0') return;
+    
+    const intBuffer = parseInt(buffer);
+
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '−') {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === '×') {
+        runningTotal *= intBuffer;
+    } else if (previousOperator === '÷') {
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleNumber(numberString) {
+    if (buffer === "0") {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
+}
+
+function init() {
+    document.querySelector('.calc-buttons').addEventListener('click', function(event) {
+        if (event.target.tagName !== 'BUTTON') return;
+        buttonClick(event.target.innerText);
+    });
+}
+
+init();
